eb4710775120dc158485e5942fcf27437bc4d055
JaecobV <23389@burnside.school.nz>
2026-05-29 20:01:29 +1200

    got the app to show the groups and info on the home page

--- app.py
+++ app.py
@@ -6,26 +6,33 @@ DATABASE = 'database.db'
 #initialises app
 app = Flask(__name__)
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
 
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+        
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 
 @app.route('/')
 def home():
     #home page
-    db = get_db()
-    cursor = db.cursor()
-    sql = "SELECT * FROM Groups;"
-    cursor.execute(sql)
-    results = cursor.fetchall()
+    sql = """
+        SELECT GroupName, TopSongs, DebutDate, GroupImage
+        FROM Groups;
+    """
+    results = query_db(sql)
     return str(results)
 
 if __name__ == "__main__":
